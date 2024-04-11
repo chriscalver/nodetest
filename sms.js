87f8d89d94821d245038e4865ecdb0aa84115332
Chris <130410868+chriscalver@users.noreply.github.com>
2024-04-11 13:15:47 -0400

    test

--- sms.js
+++ sms.js
@@ -2,9 +2,10 @@ const accountSid = 'ACf76bded57f46773c000ffad822e56c99';
 const authToken = 'bacbc31ad47193403298ca0dfa13e881';
 const client = require('twilio')(accountSid, authToken);
 
+
 client.messages
   .create({
-     body: 'Fuck you2',
+     body: 'Fuck you3',
      from: '+12512701067',
      to: '+15197099549'
    })
